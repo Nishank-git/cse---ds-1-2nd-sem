414262068f9b1130cd53ba603970a80a50f08201
Nishank-git <ashokpal14@outlook.com>
2026-04-16 13:50:55 +0530

    Initial commit

---
+++ exp6.js
@@ -0,0 +1,5 @@
+let heading = document.getElementById ("heading");
+let paragraph = document.getElementById("paragraph");
+let color = document.getElementById("change colour");
+let fontsize = 16;4
+document.getElementById("button primary").addEventListener("click", function()) {
